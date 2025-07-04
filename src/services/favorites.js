43cf2c6bb120876b19576544cbf1df0d19a644ae
RohanSkaria <rohan.skaria@icloud.com>
2025-07-04 11:37:37 -0500

    fix: styling for stars pushing down movie card

---
+++ src/services/favorites.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+class FavoritesDataService {
+    getFavorites(userId) {
+        return axios.get(`${process.env.REACT_APP_API_BASE_URL}/api/v1/movies/favorites/${userId}`);
+    }
+
+    updateFavorites(userId, favorites) {
+        return axios.put(`${process.env.REACT_APP_API_BASE_URL}/api/v1/movies/favorites`, {
+            user_id: userId,
+            favorites: favorites
+        });
+    }
+}
+
+export default new FavoritesDataService();
